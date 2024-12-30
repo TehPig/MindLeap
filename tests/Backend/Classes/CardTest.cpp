c66875f90931c70eb6aaad25b8886146c589ab90
TehPig_YT <aggelosdellas@gmail.com>
2024-12-30 15:20:04 +0200

    Add updated files

--- tests/Backend/Classes/CardTest.cpp
+++ tests/Backend/Classes/CardTest.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include <Backend/Libraries/catch.hpp>
 
-// Example test case -
+// Example test case
 TEST_CASE("Example Test", "[example]") {
     REQUIRE(1 + 1 == 2);
 }
